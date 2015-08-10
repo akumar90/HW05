524ad992b2b4a1544978d94819ce8fa90be61b07
akumar90 <ankurkumar@berkeley.edu>
2015-08-10 12:49:49 -0700

    Improving readability and exception handling

--- HW05_ex00_logics.py
+++ HW05_ex00_logics.py
@@ -14,7 +14,7 @@ def even_odd():
         returns None
     """
 
-    usrInput = raw_input("Enter the number to be determined even or odd")
+    usrInput = raw_input("Enter the number to be determined even or odd : ")
 
     try :
 
@@ -23,33 +23,43 @@ def even_odd():
 
         elif (int(usrInput) % 2 == 0):
             print ("Even number entered")
+            print 
 
         else :
             print ("Odd number entered")
+            print 
 
 
     except ValueError as ve:
         print ("Incorrect value entered. Exiting the program")
-        sys.exit()
+        print 
 
 
     return None
 
 def ten_by_ten():
     """ Prints integers 1 through 100 sequentially in a ten by ten grid."""
-    
+    print
+    print("Printing Grid")
+    print
     for i in range(0,10):
         for j in range (0,11):
             print str(i*10 + j) + "\t",
 
         print 
 
+    print
+    print
+
 
 def find_average():
     """ Takes numeric input (non-word numerals) from the user, one number
     at a time. Once the user types 'done', returns the average.
-    
+
     """
+    print
+    print ("Calculate average")
+    print 
     sum = 0
     count = 0
     while True:
@@ -64,9 +74,12 @@ def find_average():
             else :
                 return "No numbers entered"
 
-        sum += int(usrInput)
-        count += 1
+        try :
+            sum += int(usrInput)
+            count += 1
 
+        except ValueError :
+            print ("Incorrect value entered. Please enter integers only or 'done'")
 
 
 ##############################################################################
@@ -77,6 +90,7 @@ def main():
     Prints the following function:
         - find_average()
     """
+    even_odd()
     ten_by_ten()
     print repr(find_average())
 
